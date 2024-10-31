fa1a96c65fad17b2e8c87465f0696ac5b0388ee9
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-10-31 12:09:28 -0400

    ambiant occlusion

--- src/components/mesh.js
+++ src/components/mesh.js
@@ -7,16 +7,22 @@ import { useGLTF, useTexture } from '@react-three/drei';
 export const Mesh = forwardRef((props, ref) => {
     const { nodes } = useGLTF('/media/bar.glb');
     const aoTexture = useTexture('/media/ao.png');
+    aoTexture.flipY = false;
 
     useFrame((state, delta, xrFrame) => {
         // executes 1/frame, so we can just directly morph the ref with a delta
-        ref.current.rotation.x += 0.01;
-        ref.current.rotation.y += 0.02;
+        // ref.current.rotation.x += 0.01;
+        // ref.current.rotation.y += 0.02;
     })
 
     return (
         <mesh ref={ref} geometry={nodes.Plane002.geometry}>
-            <meshPhysicalMaterial roughness={0.75} map={aoTexture} />
+            <meshPhysicalMaterial
+              roughness={0.65}
+              map={aoTexture}
+              aoMap={aoTexture}
+              aoMapIntensity={0.75}
+            />
         </mesh>
     )
 })
