c3b24789b61de156da896bf8a904130f881b2b57
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-01 09:43:18 -0400

    Add spotlight

--- src/components/model.js
+++ src/components/model.js
@@ -14,15 +14,25 @@ const View = dynamic(() => import('src/components/view')
 
 export function Model(props) {
     const meshRef = useRef();
+    const slTargetRef = useRef();
 
     return (
         <View orbit {...props}>
             <Suspense fallback={null}>
                 <Mesh
                   ref={meshRef} />
-                <OrthographicCamera makeDefault position={[2, 2, 2]} near={-1000} far={1000} />
+                <OrthographicCamera makeDefault position={[2, 2, 2]} near={-2000} far={2000} />
                 <ambientLight color={0xffffff} intensity={0.7} />
-                {/* <directionalLight intensity={0.5} position={[0.5, 0, 0.866]} /> /* ~60º */}
+                <spotLight
+                  angle={1}
+                  color={0xffe9e9}
+                  decay={0.7}
+                  distance={3000}
+                  intensity={300}
+                  penumbra={1.5}
+                  position={[-80, 200, -80]}
+                  target-position={[0, -80, 200]}
+                />
             </Suspense>
         </View>
     )
