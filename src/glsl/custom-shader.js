d805b61d2bbb55f203cf9420fb3b3c063b5ecd6f
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-07 16:21:00 -0500

    Remove unused uniform. Switch to uFBO. Multiply vHeight by 40 (because our cube is multiplie by 40).

--- src/glsl/custom-shader.js
+++ src/glsl/custom-shader.js
@@ -6,25 +6,31 @@
  * and augmenting it with our own shaders and uniforms.
  */ 
 export function customizeShaders (shader) {
-    console.log(shader)
+
     // add our uniforms to the vertex shader
     shader.vertexShader = shader.vertexShader.replace(
         '#include <common>',
         /* glsl */ `
-            #include <common>
+            // #include <common>
+            uniform sampler2D uFBO;
             uniform float time;
-            uniform sampler2D uT_transition;
-            uniform sampler2D uT_displacement;
-            uniform sampler2D uT_shadow;
-            uniform float height_in;
-            uniform float height_graph_in;
-            uniform float height_graph_out;
-            uniform float height_out;
-            uniform float shadow_in;
-            uniform float shadow_out;
-            uniform float displacement_based_scale;
-            uniform float transition_noise;
-            uniform float height;
+            uniform vec3 light_color;
+            uniform vec3 ramp_color_one;
+            uniform vec3 ramp_color_two;
+            uniform vec3 ramp_color_three;
+            uniform vec3 ramp_color_four;
+            // uniform sampler2D uT_transition;
+            // uniform sampler2D uT_displacement;
+            // uniform sampler2D uT_shadow;
+            // uniform float height_in;
+            // uniform float height_graph_in;
+            // uniform float height_graph_out;
+            // uniform float height_out;
+            // uniform float shadow_in;
+            // uniform float shadow_out;
+            // uniform float displacement_based_scale;
+            // uniform float transition_noise;
+            // uniform float height;
 
             attribute vec2 instanceUV;
             varying float vHeight;
@@ -39,9 +45,11 @@ export function customizeShaders (shader) {
         /* glsl */ `
             #include <begin_vertex>
             vHeightUv = clamp(position.y * 2., 0., 1.);
-            vec4 transition = texture2D(uT_transition, instanceUV);
+            vec4 transition = texture2D(uFBO, instanceUV);
             transformed *= transition.g;
-            vHeight = transformed.y;
+            // we have to multiply by 40 here because of our cube height
+            // not sure why he didn't have to do that in the videos
+            vHeight = transformed.y * 40.;
         `
     )
 
@@ -66,8 +74,11 @@ export function customizeShaders (shader) {
             #include <color_fragment>
             // diffuseColor.rgb = vec3(1.,0.,0.);
             vec3 highlight = mix(ramp_color_three, ramp_color_four, vHeightUv);
+            // diffuseColor.rgb = highlight;
             diffuseColor.rgb = ramp_color_two;
             diffuseColor.rgb = mix(diffuseColor.rgb, ramp_color_three, vHeightUv);
+            // diffuseColor.rgb = mix(diffuseColor.rgb, highlight, vHeight);
+            // diffuseColor.rgb = mix(diffuseColor.rgb, highlight, clamp(vHeight / 10., 0., 1.));
             diffuseColor.rgb = mix(diffuseColor.rgb, highlight, clamp(vHeight / 10. - 3., 0., 1.));
         `
     )
