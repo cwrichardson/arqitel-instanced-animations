23a8142f7f1724734b43e5f5d4d8d6cd90f80a24
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-07 16:38:27 -0500

    Add something to the color to confirm that we're now dynamic.

--- src/glsl/fragment.js
+++ src/glsl/fragment.js
@@ -7,7 +7,7 @@ export const fragment = /* glsl */ `
     void main() {
         // gl_FragColor = vec4(vUv, 0., 1.);
         // gl_FragColor = vec4(1.0, 0., 0., 1.);
-        vec4 color = texture2D(uState1, vUv);
+        vec4 color = texture2D(uState1, vUv + vec2(0.0, 0.3));
         gl_FragColor = color;
     }
 `;
