154f120015f44f5de5b8f2e183c097287560d6ff
Christopher Richardson <cwr@Christophers-MacBook-Pro.local>
2024-11-07 16:32:19 -0500

    Switch to using Drei Instance.

--- src/components/mesh.js
+++ src/components/mesh.js
@@ -1,9 +1,13 @@
 'use client';
 
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { Object3D } from 'three';
+import {
+    forwardRef,
+    useImperativeHandle,
+    useMemo,
+    useRef
+} from 'react';
 import { useFrame } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
+import { Instance, Instances, useGLTF } from '@react-three/drei';
 
 import { Material } from '@/components/material';
 
@@ -19,11 +23,9 @@ export const Mesh = forwardRef((props, ref) => {
     // instance geometry
     const { nodes } = useGLTF('/media/bar.glb');
     const barGeometry = nodes.Plane002.geometry;
-    const instanceGeometry = barGeometry.scale(40, 40, 40);
 
     // instances
     const iSize = 50;
-    const instances = iSize**2;
 
     /**
      * To set the position of each instance, `instanceBufferAttribute`
@@ -40,27 +42,28 @@ export const Mesh = forwardRef((props, ref) => {
      * We did a `scale` on the underlying geometry of 40, so we use a
      * width of 60 to position each instance with a little space.
      */
-    const d = new Object3D();
     const width = 60;
-    const positions = [];
-    const instancePositions = [];
+    
+    const [instances, instanceUVs] = useMemo(() => {
+        let instances = [];
+        let instanceUVs = [];
 
-    for (let i = 0; i < iSize; i++) {
-        for (let j = 0; j < iSize; j++) {
-            d.position.set(
-                width * (i - iSize / 2),
-                0,
-                width * (j - iSize / 2)
-            );
-            d.updateMatrix();
-            positions.push(...d.matrix.elements);
-
-            // create an array for UVs
-            instancePositions.push(i / iSize, j / iSize);
+        for (let i = 0; i < iSize; i++) {
+            for (let j = 0; j < iSize; j++) {
+                const position = [
+                    width * (i - iSize / 2),
+                    0,
+                    width * (j - iSize / 2)
+                ];
+                instances.push({position});
+    
+                // create an array for UVs
+                instanceUVs.push(i / iSize, j / iSize);
+            }
         }
-    }
-    const instanceLocMatrixPositions = new Float32Array(positions);
-    const instanceUV = new Float32Array(instancePositions);
+
+        return [instances, new Float32Array(instanceUVs)];
+    }, [width, iSize]);
 
     useFrame((state, delta, xrFrame) => {
         // executes 1/frame, so we can just directly morph the ref with a delta
@@ -69,11 +72,21 @@ export const Mesh = forwardRef((props, ref) => {
     })
 
     return (
-        <instancedMesh ref={instanceMeshRef} geometry={instanceGeometry} count={instances} {...props}>
+        <Instances ref={instanceMeshRef} limit={iSize**2} {...props}>
+            <bufferGeometry {...barGeometry} />
             <Material ref={materialRef} />
-            <instancedBufferAttribute attach={'instanceMatrix'} args={[instanceLocMatrixPositions, 16]} />
-            <instancedBufferAttribute attach={'geometry-attributes-instanceUV'} args={[instanceUV, 2]} />
-        </instancedMesh>
+            <instancedBufferAttribute
+                attach={'geometry-attributes-instanceUV'}
+                args={[instanceUVs, 2]}
+            />
+            {instances.map((instance, i) => 
+                <Instance
+                  key={i}
+                  scale={40}
+                  position={instance.position}
+                />
+            )}
+        </Instances>
     )
 })
 
